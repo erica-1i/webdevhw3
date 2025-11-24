e42f7e168ffabca2c743f5af7d2a5052e23329e2
Erica Li <erica.li65@myhunter.cuny.edu>
2025-11-23 19:16:48 -0500

    edit

--- starbucks-react/src/main.jsx
+++ starbucks-react/src/main.jsx
@@ -7,7 +7,7 @@ import App from "./App.jsx";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <BrowserRouter>
+    <BrowserRouter  basename="/webdevhw3">
       <App />
     </BrowserRouter>
   </React.StrictMode>
